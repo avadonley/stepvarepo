98ba1a80b76acc45c1d4d9ddece8ee2f25b73f27
Ava Donley <ava@blainedonley.com>
2024-10-28 11:05:14 -0400

    AAAAAAA

--- index.php
+++ index.php
@@ -72,13 +72,22 @@
                 <div class="dashboard-item" data-link="calendar.php">
                     <img src="images/view-calendar.svg">
                     <span>View Calendar</span>
-                </div>
+                </div>  
+                <?php if ($_SESSION['access_level'] >= 2): ?>
+                    <div class="dashboard-item" data-link="eventSignUp.php">
+                        <img src="images/new-event.svg">
+                        <span>Sign-Up for Event</span>
+                    </div>
+                <?php endif ?>
+				
                 
-                <!--
-				<div class="dashboard-item" data-link="addService.php">
-                    <img src="images/settings.png">
-                    <span>Add Service</span>
+                
+				<div class="dashboard-item" data-link="editHours.php">
+                    <img src="images/manage-account.png">
+                    <span>Change Volunteer Hours</span>
                 </div>
+
+                <!--
 				<div class="dashboard-item" data-link="addLocation.php">
                     <img src="images/settings.png">
                     <span>Add Location</span>
@@ -97,6 +106,10 @@
                 </div>
                 -->
                 <?php if ($_SESSION['access_level'] >= 2): ?>
+                    <div class="dashboard-item" data-link="addEvent.php">
+                        <img src="images/new-event.svg">
+                        <span>Add Event</span>
+                    </div>
                     <div class="dashboard-item" data-link="personSearch.php">
                         <img src="images/person-search.svg">
                         <span>Find Volunteer</span>
