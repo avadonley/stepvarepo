a654bf69aec211d4568f3a6bcd6a04e3afccdefe
bluelightspirit <bluelittlebears@hotmail.com>
2024-10-28 20:37:03 -0400

    Update index.php

--- index.php
+++ index.php
@@ -50,39 +50,38 @@
             <p>Today is <?php echo date('l, F j, Y'); ?>.</p>
             <div id="dashboard">
                 <?php
+                    /*@ removing messages
                     require_once('database/dbMessages.php');
                     $unreadMessageCount = get_user_unread_count($person->get_id());
                     $inboxIcon = 'inbox.svg';
                     if ($unreadMessageCount) {
                         $inboxIcon = 'inbox-unread.svg';
                     }
+                    */
                 ?>
+                <!--@ removing messages
                 <div class="dashboard-item" data-link="inbox.php">
-                    <img src="images/<?php echo $inboxIcon ?>">
-                    <span>Notifications<?php 
+                    <img src="images/<?php/* echo $inboxIcon */?>">
+                    <span>Notifications<?php/* 
                         if ($unreadMessageCount > 0) {
                             echo ' (' . $unreadMessageCount . ')';
                         }
-                    ?></span>
+                    */?></span>
                 </div>
+                -->
                 <div class="dashboard-item" data-link="calendar.php">
                     <img src="images/view-calendar.svg">
                     <span>View Calendar</span>
+
+                </div>			
+                
+                
+				<div class="dashboard-item" data-link="editHours.php">
+                    <img src="images/manage-account.png">
+                    <span>Change Volunteer Hours</span>
                 </div>
-                <?php if ($_SESSION['access_level'] >= 2): ?>
-                    <div class="dashboard-item" data-link="eventSignUp.php">
-                        <img src="images/new-event.svg">
-                        <span>Sign-Up for Event</span>
-                    </div>
-                <?php endif ?>
-				<div class="dashboard-item" data-link="addEvent.php">
-                    <img src="images/new-event.svg">
-                    <span>Add Event</span>
-                </div>
-				<div class="dashboard-item" data-link="addService.php">
-                    <img src="images/settings.png">
-                    <span>Add Service</span>
-                </div>
+
+                <!--
 				<div class="dashboard-item" data-link="addLocation.php">
                     <img src="images/settings.png">
                     <span>Add Location</span>
@@ -91,6 +90,9 @@
                         <img src="images/person-search.svg">
                         <span>Find Animal</span>
                 </div>
+
+                -->
+
                 <!-- Commenting out because volunteers won't be searching events
                 <div class="dashboard-item" data-link="eventSearch.php">
                     <img src="images/search.svg">
@@ -98,6 +100,10 @@
                 </div>
                 -->
                 <?php if ($_SESSION['access_level'] >= 2): ?>
+                    <div class="dashboard-item" data-link="addEvent.php">
+                        <img src="images/new-event.svg">
+                        <span>Add Event</span>
+                    </div>
                     <div class="dashboard-item" data-link="personSearch.php">
                         <img src="images/person-search.svg">
                         <span>Find Volunteer</span>
@@ -106,14 +112,22 @@
                         <img src="images/add-person.svg">
                         <span>Register Volunteer</span>
                     </div>
+                    <div class="dashboard-item" data-link="addEvent.php">
+                        <img src="images/new-event.svg">
+                        <span>Add Event</span>
+                    </div>
+
+                    <!--
                     <div class="dashboard-item" data-link="viewArchived.php">
                         <img src="images/person-search.svg">
                         <span>Archived Animals</span>
                     </div>
+                
                     <div class="dashboard-item" data-link="report.php">
                         <img src="images/create-report.svg">
                         <span>Create Report</span>
                     </div>
+                -->
                 <?php endif ?>
                 <?php if ($notRoot) : ?>
                     <div class="dashboard-item" data-link="viewProfile.php">
@@ -130,6 +144,10 @@
                         <img src="images/volunteer-history.svg">
                         <span>View My Hours</span>
                     </div>
+                    <div class="dashboard-item" data-link="eventSignUp.php">
+                    <img src="images/new-event.svg">
+                    <span>Sign-Up for Event</span>
+                </div>
                 <?php endif ?>
                 <div class="dashboard-item" data-link="changePassword.php">
                     <img src="images/change-password.svg">
