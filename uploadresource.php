83ff762c3dc9f377fd2ef25fe76b55c8c7dee684
iqureshi223 <iqureshi@gmail.com>
2024-11-11 19:27:25 -0500

    Created uploadresource.php

---
+++ uploadresource.php
@@ -0,0 +1,18 @@
+<?php
+    session_cache_expire(30);
+    session_start();
+
+    include_once('database/dbinfo.php');
+    include_once('database/dbPersons.php');
+    include_once('domain/Person.php');
+    include_once('database/dbEvents.php');
+
+    include_once('database/dbShifts.php');
+    include_once('domain/Shift.php');
+?>
+
+<html>
+    <head>
+        
+    </head>
+</html>
