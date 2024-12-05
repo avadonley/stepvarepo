f98c261bb3147c6ab5f6711119ff61f4b941c8c1
Thomas Held <theld144@gmail.com>
2024-12-05 08:36:37 -0500

    restored delete check-in function in dbPersons

--- database/dbPersons.php
+++ database/dbPersons.php
@@ -260,6 +260,15 @@ function fetch_volunteering_hours($personID, $eventID) {
     return -1; // no check-ins found
 }
 
+
+/* Delete a single check-in/check-out pair as defined by the given parameters */
+function delete_check_in($userID, $eventID, $start_time, $end_time) {
+    $con=connect();
+    $query = "DELETE FROM dbpersonhours WHERE personID = '" .$userID. "' AND eventID = '" .$eventID. "' AND start_time = '" .$start_time. "' AND end_time = '" .$end_time. "' LIMIT 1";
+    $result = mysqli_query($con, $query);
+    mysqli_close($con);
+}
+
 /*@@@ end Thomas */
 
 /*
